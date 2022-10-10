cd49cdf3480a096771b63e3ad7c8cb4a80407aff
BarracudaPff <mrm.kikll@gmail.com>
2022-10-10 18:46:07 +0200

    add comments

--- src/main/kotlin/ru/otus/otuskotlin/exercise3/SampleForTasks.kt
+++ src/main/kotlin/ru/otus/otuskotlin/exercise3/SampleForTasks.kt
@@ -2,12 +2,13 @@ package ru.otus.otuskotlin.exercise3
 
 import java.util.*
 
+// Will not work in play.kotlinlang.org, but works in IDE
 fun main() {
-    val input = Scanner(System.`in`)
-    val a = input.nextInt()
-    val b = input.nextInt()
+    val input = Scanner(System.`in`) // init scanner
+    val a = input.nextInt() // read integer from console
+    val b = input.nextInt() // read another integer from console
 
-    val res = a + b
+    val res = a + b // compute result
 
-    println(res)
+    println(res) // output result
 }
