87b6f0d72ccd0ef740808bf0af640a9db1aadfe3
DTadescu <tada.mascot@gmail.com>
2023-01-06 19:51:14 +0300

    M6L5 Роутинг и репозиторий

--- rest-lesson/src/main/kotlin/basic/otuskotlin/otus/ru/Application.kt
+++ rest-lesson/src/main/kotlin/basic/otuskotlin/otus/ru/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import basic.otuskotlin.otus.ru.plugins.*
+import basic.otuskotlin.otus.ru.repo.CatsRepositoryInMemory
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -11,7 +12,11 @@ fun main() {
 }
 
 fun Application.module() {
+
+    // инициализация репозитория для доступа к данным
+    val repo = CatsRepositoryInMemory()
+
     configureHTTP()
     configureSerialization()
-    configureRouting()
+    configureRouting(repo)
 }
